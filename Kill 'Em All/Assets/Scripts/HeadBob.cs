170c9e94e7288220c1f0ddd9f20b9fbe51e3709a
NoahWilson <noahwilsonmailbox32@gmail.com>
2021-04-28 19:08:39 -0400

    Head Bob
    Added head bobbing

---
+++ Kill 'Em All/Assets/Scripts/HeadBob.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBob : MonoBehaviour
+{
+    public float bobSpeed = 14f;
+    public float bobAmount = .05f;
+
+    float defaultZ = 0;
+    float timer = 0;
+
+    public PlayerController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultZ = transform.localPosition.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > .1f || Mathf.Abs(Input.GetAxis("Vertical")) > .1f)
+        {
+            timer += Time.deltaTime * bobSpeed;
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, defaultZ + Mathf.Sin(timer) * bobAmount);
+        }
+        else
+        {
+            timer = 0;
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, Mathf.Lerp(transform.localPosition.z, defaultZ, Time.deltaTime * bobSpeed));
+        }
+    }
+}
